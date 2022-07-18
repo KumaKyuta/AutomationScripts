da0190fc4657864b6e1e7315c3ec3bc2bcc0c291
Dwayne Lacey <36286398+KumaKyuta@users.noreply.github.com>
2022-07-18 19:43:11 -0400

    Imported tkinter, return directory based on user choice

--- dateupdate.py
+++ dateupdate.py
@@ -1,1 +1,10 @@
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
 
+# Prevents root window from appearing
+Tk().withdraw()
+
+# Choose current working directory to use
+current_directory = askdirectory()
+
+print(current_directory)
